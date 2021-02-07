06f9f1db336c232814b61a924f975e390065ae7f
clintonoliera <clintonoliera391@gmail.com>
2021-02-07 03:47:14 +0300

    initial commit and setup

---
+++ Accounts/urls.py
@@ -0,0 +1,17 @@
+from .views import Home,Signup,Login,logout,Cart,Checkout,OrderView 
+from .auth import LoginCheckMiddleware,LogoutCheckMiddleware
+from django.urls import path,include
+from django.contrib.auth import views as as_view
+from . import views
+
+
+
+urlpatterns = [
+    path('',Home.as_view(), name='home'),
+    path('logout',LoginCheckMiddleware(logout), name='logout'),
+    path('cart',Cart.as_view(), name='cart'),
+    path('signup',LogoutCheckMiddleware(Signup.as_view()), name='signup'),
+    path('login',LogoutCheckMiddleware(Login.as_view()), name='login'),
+    path('checkout',LoginCheckMiddleware(Checkout.as_view()), name='checkout'),
+    path('order',LoginCheckMiddleware(OrderView.as_view()), name='order'),
+]
